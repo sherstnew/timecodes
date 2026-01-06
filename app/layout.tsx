f8ff217d2b2c014c02830c1856f6ed7737309d25
sherstnew <sherstnev.denis.v@gmail.com>
2026-01-06 22:22:12 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="referrer" content="no-referrer" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
